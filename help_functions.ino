2ab2536fbe55d7e83396b8def374a3750fd5cf71
Vadym Salymonenko <vadym95201@gmail.com>
2023-12-09 22:32:34 +0200

    sedfzsfszfz

--- help_functions.ino
+++ help_functions.ino
@@ -605,7 +605,7 @@ void atack_round_2() {
                  // Serial.print("getFrontInfraredDistance() == ");
                  // Serial.println(getFrontInfraredDistance());
                   //continue;
-                  int rear_distance = getRearInfraredDistance_array_5();
+                  int rear_distance = getRearInfraredDistance();
                   if(rear_distance < (TRACK_DISTANCE_SENSORS/10 + 5)){
                     delay(5);
                         if(rear_distance <= 55){
@@ -614,13 +614,13 @@ void atack_round_2() {
                             start_ONE_TurnLeft(turn_speed+45, 1.40);
                         }
                     }
-                  result = getFrontInfraredDistance_array_5();
+                  result = getFrontInfraredDistance();
                   Serial.print("result >> ");
                       Serial.println(result);
                   if(result < TRACK_DISTANCE_SENSORS/10){
                      Serial.println("check 1111");
                     delay(8);
-                    result = getFrontInfraredDistance_array_5();
+                    result = getFrontInfraredDistance();
                     Serial.print("check 2 >>");
                     Serial.println(result);
                     if(result >= TRACK_DISTANCE_SENSORS/10){
