53719ecc0a5f0fdacb0983df0eec8dc476f3e20f
Vadym Salymonenko <vadym95201@gmail.com>
2023-12-13 23:15:02 +0200

    vcvffghg

--- help_functions.ino
+++ help_functions.ino
@@ -641,7 +641,7 @@ void atack_round_2() {
   if(getFrontInfraredDistance_array_5() <= TRACK_DISTANCE_SENSORS/10){ 
     stage = 2;
   }else{
-  startQuickTurnLeft(turn_speed);
+  startQuickTurnLeft(35);
   }
 
     while(millis() - round_length_time <= TOTAL_ROUND_LENGTH){
