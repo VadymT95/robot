acedd2408521f036187ee6078a0d10914747e79c
Vadym Salymonenko <vadym95201@gmail.com>
2023-12-13 22:59:31 +0200

    rtrtrtrtr

--- help_functions.ino
+++ help_functions.ino
@@ -203,7 +203,7 @@ void init_color_sensors(){
   #endif
 }
 void set_right_motor_additional_boost_CHECK(){
-  if((d1_filtred <= 120 || d2_filtred <= 120) && right_motor_add_boost_permit == 1)
+  if((d1_filtred <= 120 || d2_filtred <= 120) && right_motor_add_boost_permit == 1 && boost_permit == 1)
   right_motor_additional_boost = RIGHT_MOTOR_ADD_BOOST_MAX_VALUE;
 }
 float calculateGain(float voltage) {
